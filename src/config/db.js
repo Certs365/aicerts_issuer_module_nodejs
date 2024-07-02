7fca2c903d33d71e7be0b7faec02857912eadedb
sandeepparimi <158067728+sandeepparimi@users.noreply.github.com>
2024-07-02 19:09:03 +0530

    update feedback

--- src/config/db.js
+++ src/config/db.js
@@ -4,26 +4,26 @@ const maxRetries = 3;
 var retryCount;
 
 function connectWithRetry() {
-try {
-  mongoose.connect(process.env.MONGODB_URI)
-      .then(() => {
-          console.log("DB Connected");
-      })
-      .catch((err) => {
-          console.log("DB Connection Failed");
-          if (retryCount < maxRetries) {
-              console.log(`Retrying... Attempt ${retryCount + 1}`);
-              retryCount++;
-              connectWithRetry();
-          } else {
-              console.log("Maximum retries exceeded. Exiting...");
-              process.exit(1); // Exit the application after max retries
-          }
-      });
+    try {
+        mongoose.connect(process.env.MONGODB_URI)
+            .then(() => {
+                console.log("DB Connected");
+            })
+            .catch((err) => {
+                console.log("DB Connection Failed", err);
+                if (retryCount < maxRetries) {
+                    console.log(`Retrying... Attempt ${retryCount + 1}`);
+                    retryCount++;
+                    connectWithRetry();
+                } else {
+                    console.log("Maximum retries exceeded. Exiting...");
+                    process.exit(1); // Exit the application after max retries
+                }
+            });
     } catch (error) {
-      console.error("Invalid MongoDB URI:", error.message);
-      process.exit(1); // Exit the application if URI is invalid
-  }
+        console.error("Invalid MongoDB URI:", error.message);
+        process.exit(1); // Exit the application if URI is invalid
+    }
 }
 
 connectWithRetry();
