5789fd5a2d2feb37ec267f1b1d9fd8e9e38aacb1
Sachin Gorakshnath Thokale <sachin.thokale@aicerts.io>
2024-10-28 22:51:52 +0530

    error handling with universal retrymechanism

--- src/controller/certificate_designer.js
+++ src/controller/certificate_designer.js
@@ -1,6 +1,7 @@
 // controllers/imageController.js
 const Image = require('../models/image');
 const uploadImageToS3 = require('../utils/upload');
+const retryOperation = require("../utils/retryOperation")
 
 // Upload image
 exports.uploadImage = async (req, res) => {
@@ -10,9 +11,8 @@ exports.uploadImage = async (req, res) => {
             return res.status(400).json({ message: "No file uploaded." });
         }
 
-        // Upload to S3
-        const uploadResult = await uploadImageToS3(req.file);
-    
+        // Retry upload to S3 up to 3 times if it fails
+        const uploadResult = await retryOperation(() => uploadImageToS3(req.file), 3, 1000);
 
         const { issuerId,type } = req.body;
         const imageUrl = uploadResult.Location; // Get the S3 URL of the uploaded image
@@ -41,6 +41,10 @@ exports.getImagesByIssuerId = async (req, res) => {
         const { issuerId } = req.params;
          // Find images where issuerId matches and imageType is 'image'
          const images = await Image.find({ issuerId, imageType: 'image' });
+
+         if (!images || images.length === 0) {
+            return res.status(404).json({ message: 'No images found for this issuer' });
+        }
         
            // Map over the images to extract the image URL and ID
            const imageDetails = images.map(image => ({
@@ -62,6 +66,10 @@ exports.getBackgroundsByIssuerId = async (req, res) => {
         const { issuerId } = req.params;
          // Find images where issuerId matches and imageType is 'image'
          const images = await Image.find({ issuerId, imageType: 'background' });
+
+         if (!images || images.length === 0) {
+            return res.status(404).json({ message: 'No backgrounds found for this issuer' });
+        }
         
         // Map over the images to extract the image URL and ID
         const imageDetails = images.map(image => ({
@@ -84,6 +92,8 @@ exports.deleteImage = async (req, res) => {
 
         // Find the image and delete it
         const image = await Image.findOneAndDelete({ _id: imageId, issuerId });
+
+        
 
         if (!image) {
             return res.status(404).json({ message: 'Image not found' });
