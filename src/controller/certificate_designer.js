8a70b727ed469ec8e74d8b6880ff5371d6c25785
sandeepparimi <158067728+sandeepparimi@users.noreply.github.com>
2024-11-19 16:47:49 +0530

    Update certificate_designer.js

--- src/controller/certificate_designer.js
+++ src/controller/certificate_designer.js
@@ -3,10 +3,14 @@ const Image = require('../models/image');
 const uploadImageToS3 = require('../utils/upload');
 const retryOperation = require("../utils/retryOperation")
 
-// Upload image
+/**
+ * API call to Upload image.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 exports.uploadImage = async (req, res) => {
     try {
-
         if (!req.file) {
             return res.status(400).json({ message: "No file uploaded." });
         }
@@ -17,8 +21,6 @@ exports.uploadImage = async (req, res) => {
         const { issuerId,type } = req.body;
         const imageUrl = uploadResult.Location; // Get the S3 URL of the uploaded image
         console.log(type)
-
-       
 
         const newImage = new Image({
             issuerId,
@@ -31,11 +33,16 @@ exports.uploadImage = async (req, res) => {
         res.status(201).json({ message: 'Image uploaded successfully', image: newImage });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error uploading image', error });
+        return res.status(500).json({ message: 'Error uploading image', error });
     }
 };
 
-// Get images by issuerId
+/**
+ * API call to Get images by issuerId.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 exports.getImagesByIssuerId = async (req, res) => {
     try {
         const { issuerId } = req.params;
@@ -53,14 +60,19 @@ exports.getImagesByIssuerId = async (req, res) => {
         }));
        
 
-        res.status(200).json(imageDetails); // Send the image URLs as the response
+        return res.status(200).json(imageDetails); // Send the image URLs as the response
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error fetching images', error });
+        return res.status(500).json({ message: 'Error fetching images', error });
     }
 };
 
-// Get images by issuerId
+/**
+ * API call to Get background images by issuerId.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 exports.getBackgroundsByIssuerId = async (req, res) => {
     try {
         const { issuerId } = req.params;
@@ -77,14 +89,23 @@ exports.getBackgroundsByIssuerId = async (req, res) => {
             imageUrl: image.imageUrl
         }));
        
-
-        res.status(200).json(imageDetails); // Send the image URLs as the response
+        return res.status(200).json(imageDetails); // Send the image URLs as the response
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error fetching images', error });
+        return res.status(500).json({ 
+            code: 500,
+            status: 'FAILED',
+            message: 'Error fetching images', error 
+        });
     }
 };
 
+/**
+ * API call to delete image by issuerId.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 // DELETE /api/delete/certificate/image/:issuerId/:imageId
 exports.deleteImage = async (req, res) => {
     try {
@@ -93,16 +114,18 @@ exports.deleteImage = async (req, res) => {
         // Find the image and delete it
         const image = await Image.findOneAndDelete({ _id: imageId, issuerId });
 
-        
-
         if (!image) {
             return res.status(404).json({ message: 'Image not found' });
         }
 
-        res.status(200).json({ message: 'Image deleted successfully' });
+        return res.status(200).json({ 
+            code: 200,
+            status: 'SUCCESS',
+            message: 'Image deleted successfully' 
+        });
     } catch (error) {
         console.error('Error deleting image:', error);
-        res.status(500).json({ message: 'Error deleting image', error });
+        return res.status(500).json({ message: 'Error deleting image', error });
     }
 };
 
